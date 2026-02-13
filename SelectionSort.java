d72468ab12366c8b6fe092361ecaeb981a8fe6e1
iafework <166059146+iafework@users.noreply.github.com>
2026-02-12 18:22:20 -0700

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,17 +1,26 @@
-public class SelectionSort {
-    public static <T extends Comparable<T>> void selectionSort(T[] array) {
+// SelectionSort uses O(n^2). This means the more data, the more time it'll take to process the data.
 
-        for(int i = 0; i < array.length - 1; i++) {
-            int min = i;
-    
+public class SelectionSort {
+
+    // This public static method is T in order to account for other data types.
+    public static <T extends Comparable<T>> void selectionSort(T[] array) {
+        
+        // Begins with 0 as a starting point to work off of all the upcoming numbers.
+        for(int i = 0; i < array.length - 1; i++) { // First index of the unsorted section.
+            int min = i; // Until a larger number is found, "i" is assumed to be the smallest.
+             
+            // The following inner loop is to scan the rest of the array and see if there's anything smaller than the current minimum.
             for(int j = i + 1; j < array.length; j++) {
                 if (array[j].compareTo(array[min]) < 0) {
                     min = j;
                 }
             }
+
+            // Swapping the variable with the larger number in the index
             T temp = array[i];
             array[i] = array[min];
-            array[min] = temp;
+            array[min] = temp;   
         }
+
     }
 }
